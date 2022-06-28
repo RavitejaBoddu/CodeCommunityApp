ad3380a270a2dcabe690139a840cf74c8cae8c22
Raviteja Boddu <raviteja1312boddu@gmail.com>
2022-06-28 11:05:22 +0530

    added CI action

--- cypress/e2e/login_negative.cy.js
+++ cypress/e2e/login_negative.cy.js
@@ -1,14 +1,26 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('#email')
-    .type('test@test.com')
+describe("empty spec", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+  it("checks with incorrect password", () => {
+    cy.get("#email").type("test@test.com");
 
-    cy.get('#pass')
-    .type('123456')
+    cy.get("#pass").type("123456");
 
-    cy.get('#login').click()
+    cy.get("#login").click();
+    cy.on("window:alert", (t) => {
+      expect(t).to.contains("wrong password");
+    });
+  });
 
-    
-  })
-})
+  it("checks with incorrect email", () => {
+    cy.get("#email").type("test@test.com");
+
+    cy.get("#pass").type("123456");
+
+    cy.get("#login").click();
+    cy.on("window:alert", (t) => {
+      expect(t).to.contains("user not found");
+    });
+  });
+});
--- cypress/e2e/login_positive.cy.js
+++ cypress/e2e/login_positive.cy.js
@@ -1,17 +1,19 @@
-describe('My First Test', () => {
-  it('evaluates Login', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('#email')
-    .type('test@test.com')
 
-    cy.get('#pass')
-    .type('12345678')
+const email = "test@test.com";
+const password ="12345678"
 
-    cy.get('#login').click()
 
-    cy.get('#user_Email').contains( 'test@test.com')
+describe("Test Login Functionality", () => {
+  it("evaluates Login with correct credentials", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("#email").type(email);
 
-    cy.get('#logout-btn').click()
+    cy.get("#pass").type(password);
 
-  })
-})
+    cy.get("#login").click();
+
+    cy.get("#user_Email").contains("test@test.com");
+
+    cy.get("#logout-btn").click();
+  });
+});
